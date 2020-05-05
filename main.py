da8c009ddea097bd6e144c9765d31cc60cd58c3b
Glitch (hello-express) <none>
2020-05-05 02:56:30 +0000

    🎢🚶 Checkpoint ./main.py:13956203/776

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("aaaaaaaaaaaa")
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("ログイン完了")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("こんにちは！"):
+        await message.channel.send("こんにちは！")
+    if message.content.startswith("よし"):
+        await message.channel.send("どうでしたか？")
+    if message.content.startswith("ふぅ"):
+        await message.channel.send("おつかれさん")
+    if message.content.startswith("ああ"):
+        await message.channel.send("どうした？")
+    if message.content.startswith("こんばんは！"):
+        await message.channel.send("こんばんは！")
+
+client.run("")
